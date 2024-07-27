cc8ef598aa1496b1bb8262d43fd9505e1e57ce41
darshilgamdha <darshilgamdha74@gmail.com>
2024-07-27 10:57:26 +0530

    up

--- 63.js
+++ 63.js
@@ -1,11 +1,11 @@
 // 63.	Write a program to find the first and last digit of a number.
-let num= 1234566;
-let firstdigit = 0;
-let lastdigit = 0;
-while(num !=0){
-    firstdigit = num % 10;
-    num = ~~(num / 10);
-    lastdigit = num % 10;
-    }
-    console.log("First digit is "+firstdigit+" and last digit is "+lastdigit);
 
+    let n =135
+    let  first
+    let  last
+
+    first = n / 1000;
+    last = n % 10;
+
+    console.log(first);
+    console.log(last);
