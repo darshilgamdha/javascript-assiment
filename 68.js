c3ee1588a91bb54ceac9537f7b8af63046fe9372
darshilgamdha <darshilgamdha74@gmail.com>
2024-07-27 14:19:36 +0530

    update

--- 68.js
+++ 68.js
@@ -1,1 +1,8 @@
 // 68.	Write a program to enter a number and print its reverse.
+let number = 1234556
+reverse = ""
+while (number != 0) {
+    let last_digit = number % 10
+    reverse = reverse + last_digit;
+    number = ~~(number / 10)
+    } console.log(reverse) 
--- 69.js
+++ 69.js
@@ -1,1 +1,19 @@
 // 69.	Write a program to check whether a number is palindrome or not.
+let n = 1214 ;
+let original = n;
+
+let reverse = 0 
+
+while( n != 0){
+    let digit = n % 10; //4 //3 //2 //1
+    reverse = reverse * 10 + digit;
+
+    n = ~~(n/10); //123 //12 //1 //0
+}
+console.log(reverse)
+
+if(reverse == original){
+    console.log("palindrome")
+} else {
+console.log("not palindrome")
+}
