8c120f92f23916df3e9fd87ea671db0e854ce0de
darshilgamdha <darshilgamdha74@gmail.com>
2024-07-30 15:06:58 +0530

    uodate

--- object/2.js
+++ object/2.js
@@ -1,10 +1,10 @@
 // Modify Object Properties:
 // Task: Given the object house = { rooms: 4, color: 'blue', location: 'city' }, write a function to update the color property to 'green'.
 
-house = {
-    "rooms": 4,
-    "color": "blue",
-    "location": "city"
+ house = {
+    rooms: 4,
+    color: "blue",
+    location: "city"
 }
 console.log(house.rooms);
 console.log(house.color);
