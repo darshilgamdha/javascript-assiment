67eb3282459026bde25dfc3dfae0d05aa45a2bc1
darshil <darshilgamdha74@gmail.com>
2025-04-16 18:49:28 +0530

    update

--- oop/constructor.js
+++ oop/constructor.js
@@ -1,23 +1,28 @@
 class RailwayForm {
-    constructor(givenname,trainno){
+    constructor(givenname,trainno,address){
         console.log("Constructor called ..." + givenname + " " + trainno);
             this.name = givenname;
             this.trainno = trainno;
+            this.address = address;
         
     }
+    preview(){
+        alert(this.name + " : Your form is for train number :" + this.trainno + " and your address is " + this.address);
+    }
     submit(){
-        alert(this.name + " form is submitted for train no " + this.trainno);
+        alert(this.name + " Your form is submitted for train number : " + this.trainno);
     }
 
     cancel(){
-        alert(this.name + "  form is cancelled for train no " + this.trainno);  
+        alert(this.name + "  This form is cancelled for train number : " + this.trainno);  
+        this.trainno = 0;   
     }
    }
-//creat a form for harry and rohan
-let harryForm = new RailwayForm("Harry", 12345);
-let rohanForm = new RailwayForm("Rohan", 54321);
+//create a form for harry
+let harryForm = new RailwayForm("Harry", 12345,"rajkot");
 
+harryForm.preview();
 harryForm.submit();
 harryForm.cancel();
-rohanForm.submit();
-rohanForm.cancel();
+harryForm.preview();
+
